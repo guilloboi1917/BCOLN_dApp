30b64cf8d9e429363a1b4f4daa619397f078e3a1
noah.isaak <noah.isaak@uzh.ch>
2025-04-01 10:18:06 +0200

    initial commit with empty hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337
+    }
+  },
+  ignition: {
+    strategyConfig: {
+      create2: {
+        salt: "esports-dapp-salt" // For deterministic deployment
+      }
+    }
+  }
+};
