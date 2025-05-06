1137098f9291c5ab3198d78ad7eb60e5ee2d9e28
imets01 <100612206+imets01@users.noreply.github.com>
2025-05-06 22:20:45 +0200

    network id change

--- bcoln_backend/hardhat.config.js
+++ bcoln_backend/hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     hardhat: {
-      chainId: 1234
+      chainId: 31337
     },
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 1234
+      chainId: 31337
     }
   },
   ignition: {
